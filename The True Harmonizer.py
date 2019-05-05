a044cf81cf030cbdc6714b46f295d6e978c0815d
Jabari D Byrd <jbyrd13@masonlive.gmu.edu>
2019-05-05 15:19:31 -0400

    Add some more music and finish splitting up high notes from bass notes in numpy array.  Start tensorflow stuff

--- The True Harmonizer.py
+++ The True Harmonizer.py
@@ -1,3 +1,38 @@
 import py_midicsv
+import numpy as np
+import tensorflow as tf
 
-csv_string = py_midicsv.midi_to_csv("C:\Users\pivit\OneDrive\Computer Science\CS 484\Music\Rock\Test.py")
+csv_string = py_midicsv.midi_to_csv(
+    "Music\Rap\Four_five_seconds_-_Rihanna_Paul_McCartney_Kanye_West.mid")
+
+information = []
+highnotes = []
+bassnotes = []
+
+for note in csv_string:
+    temp = note.split(',')
+    for attribute in temp:
+        if attribute == '1':
+            for check in temp:
+                if check == ' Note_on_c':
+                    highnotes.append(temp)
+        elif attribute == '2':
+            for check in temp:
+                if check == ' Note_on_c':
+                    bassnotes.append(temp)
+        else:
+            temp=temp
+
+highnumpynotes = np.array(highnotes)
+bassnumpynotes = np.array(bassnotes)
+print(highnumpynotes.shape)
+print(bassnumpynotes.shape)
+print(bassnumpynotes)
+
+LOGDIR = './graphs'
+
+sess = tf.Session()
+
+LEARNING_RATE = 0.01
+BATCH_SIZE = 1000
+EPOCHS = 10
